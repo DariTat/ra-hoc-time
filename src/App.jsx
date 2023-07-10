e36de8df69add4fc6d91762964f1b0da9457578d
Daria Tatarinova <dt.dashenka@mail.ru>
2023-07-10 21:33:22 +1000

    init

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import './App.css'
+import moment from 'moment';
+import React, {useState} from 'react';
+
+function DateTime(props) {
+  return (
+    <p className="date">{props.date}</p>
+  )
+}
+
+function DateTimePretty(Component) {
+  return class extends React.Component {
+    render() {
+      const { date } = this.props;
+      moment.locale('ru');
+      const dateDiff = {
+        date: moment(date).fromNow()
+      }
+
+      return(
+        <Component {...this.props} {...dateDiff} />
+      )
+    }
+  }
+}
+
+const NewDateTime = DateTimePretty(DateTime);
+
+function Video(props) {
+  return (
+    <div className="video">
+      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <NewDateTime date={props.date} />
+    </div>
+  )
+}
+
+function VideoList(props) {
+  return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
+
+function App() {
+  
+  const [list, setList] = useState([
+    {
+        url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+        date: '2017-07-31 13:24:00'
+    },
+    {
+        url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+        date: '2018-03-03 12:10:00'
+    },
+    {
+        url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+        date: '2018-02-03 23:16:00'
+    },
+    {
+        url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+        date: '2018-01-03 12:10:00'
+    },
+    {
+        url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+        date: '2018-01-01 16:17:00'
+    },
+    {
+        url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+        date: '2017-12-02 05:24:00'
+    },
+]);
+
+  return (
+    <>
+      <VideoList list={list} />
+    </>
+  )
+}
+
+export default App
